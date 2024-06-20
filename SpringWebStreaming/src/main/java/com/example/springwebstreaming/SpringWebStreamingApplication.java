ce991d738d23cca11df4c2f48ca393be31642882
Kapparov Murat <paprim2003@gmail.com>
2024-06-20 14:41:11 +0500

    project init and video streaming for study purposes

---
+++ SpringWebStreaming/src/main/java/com/example/springwebstreaming/SpringWebStreamingApplication.java
@@ -0,0 +1,28 @@
+package com.example.springwebstreaming;
+
+import com.example.springwebstreaming.service.StreamingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class SpringWebStreamingApplication {
+    @Autowired
+    private StreamingService service;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebStreamingApplication.class, args);
+    }
+    @GetMapping(value = "video/{title}",produces = "video/mp4")
+    public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range){
+        System.out.println("range by bytes() : " + range);
+        return service.getVideo(title);
+    }
+}
